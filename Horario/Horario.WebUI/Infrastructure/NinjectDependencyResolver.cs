4a7a098c4a48bc01cd31e7ed7f02ca0d7ff37a1e
Josueemx <josueemx@gmail.com>
2016-04-26 16:13:11 -0600

    2
    blah

---
+++ Horario/Horario.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+
+namespace Horario.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+            //tip: poner el raton sobre cada método invocado, dice qué hace
+        }
+
+        private void AddBindings()
+        {
+            //////////////aquí volveremos a cada rato
+        }
+
+
+    }
+
+}
